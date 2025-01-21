3841b743fe0c376225ba0e45520397091b8044aa
Daniel Poss <rixxroid@gmail.com>
2025-01-20 23:57:42 -0600

    Update inject-into-task-def.sh to accept task JSON path and modify release workflow to pass it

--- pipelineScripts/inject-into-task-def.sh
+++ pipelineScripts/inject-into-task-def.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Check if the correct number of arguments are provided
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <CPU> <MEMORY> <EXECUTION_ROLE_ARN>"
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 <CPU> <MEMORY> <EXECUTION_ROLE_ARN> <TASK_JSON_PATH>"
     exit 1
 fi
 
@@ -10,8 +10,9 @@ fi
 CPU=$1
 MEMORY=$2
 EXECUTION_ROLE_ARN=$3
+TASK_JSON_PATH=$4
 
 # Use jq to update the task definition
 jq --arg CPU "$CPU" --arg MEMORY "$MEMORY" --arg EXEC_ROLE_ARN "$EXECUTION_ROLE_ARN" \
    '.containerDefinitions[0].cpu = $CPU | .containerDefinitions[0].memory = $MEMORY | .executionRoleArn = $EXEC_ROLE_ARN' \
-   ../ecs/task-definition.json > ../ecs/task-definition-updated.json
+   "$TASK_JSON_PATH" > "${TASK_JSON_PATH%.json}-updated.json"
